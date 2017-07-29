917ab72df921ddc7ec0fae82eac693a4a1fc708a
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2017-07-29 13:16:15 -0300

    iniciado o projeto

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import easyaccept.EasyAccept;
+
+public class Main {
+
+	public static void main(String[] args) {
+		args = new String[] { "projeto.Facade", "us/us1_test.txt", 
+				"us/us2_test.txt", "us/us3_test.txt",
+				"us/us4_test.txt", "us/us5_test.txt" };
+		EasyAccept.main(args);
+	}
+}
