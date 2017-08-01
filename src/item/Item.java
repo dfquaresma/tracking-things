bfff580b4572d9ad3b22fb781064d878595e4688
Ícaro Lima <icaro.lima@ccc.ufcg.edu.br>
2017-08-01 20:36:08 -0300

    Adicionado atributo booleano "emprestado", bem como seu método isEmprestado e setEmprestado.

--- src/item/Item.java
+++ src/item/Item.java
@@ -1,13 +1,16 @@
 package item;
 
 public abstract class Item {
-	
+
 	private String nome;
 	private double valor;
+	private boolean emprestado;
 
 	public Item(String nome, double valor) {
 		this.nome = nome;
 		this.valor = valor;
+
+		this.emprestado = false;
 	}
 
 	public String getNome() {
@@ -25,7 +28,13 @@ public abstract class Item {
 	public void setValor(double valor) {
 		this.valor = valor;
 	}
-	
-	
+
+	public boolean isEmprestado() {
+		return this.emprestado;
+	}
+
+	public void setEmprestado(boolean emprestado) {
+		this.emprestado = emprestado;
+	}
 
 }
