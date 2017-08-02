d2fca93e136a37aa9f39e68110411c288ac5c095
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2017-08-01 23:37:18 -0300

    corrigindo meu merge

--- src/sistema/Sistema.java
+++ src/sistema/Sistema.java
@@ -11,7 +11,117 @@ public class Sistema {
 	public Sistema() {
 		this.usuarios = new HashMap<>();
 	}
+
+	public void cadastrarUsuario() {
+		
+	}
+		
+	public void removerUsuario() {
+		
+	}
+	
+	public void atualizarUsuario() {
+		
+	}
+	
+	public void getInfoUsuario(){
+		
+	}
 	
 	
+	public void cadastrarEletronico() {
+		
+	}
+	
+	public void cadastrarJogoTabuleiro() {
+		
+	}
+	
+	public void adicionarPecaPerdida() {
+		
+	}
+	
+	public void cadastrarBluRayFilme() {
+		
+	}
+	
+	public void cadastrarBluRayShow() {
+		
+	}
+	
+	public void cadastrarBluRaySerie() {
+		
+	}
+
+	public void adicionarBluRay() {
+		
+	}
+	
+	public void removerItem() {
+		
+	}
+	
+	public void atualizarItem() {
+		
+	}
+	
+	public void getInfoItem() {
+		
+	}
+	
+	
+	public void listarItensOrdenadosPorNome() {
+		
+	}
+	
+	public void listarItensOrdenadosPorValor() {
+		
+	}
+	
+	public void pesquisarDetalhesItem() {
+		
+	}
+	
+	
+	public void registrarEmprestimo() {
+		
+	}
+	
+	public void devolverItem() {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
 
 }
