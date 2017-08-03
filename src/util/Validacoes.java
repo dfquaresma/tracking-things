87f00959886b494ebedf3b07e504f47da0961718
xdLuna <lunaft9@gmail.com>
2017-08-03 17:48:02 -0300

    Classe Validacoes ajeitada.

--- src/util/Validacoes.java
+++ src/util/Validacoes.java
@@ -2,49 +2,33 @@ package util;
 
 public class Validacoes {
 	
+	private void validaString(String atributo, String atributoMsg){
+		if(atributo == null){
+			throw new NullPointerException(atributoMsg + " nao pode ser nulo");
+		}
+		if(atributo.trim().isEmpty()){
+			throw new IllegalArgumentException(atributoMsg + " nao pode ser vazio");
+		}
+	}
+	
 	public void validaNome(String nome){
-		if(nome.trim().isEmpty()){
-			throw new IllegalArgumentException("Nome nao pode ser vazio");
-		}
-		if(nome == null){
-			throw new NullPointerException("Nome nao pode ser nulo");
-		}
+		this.validaString(nome, "Nome");
 	}
 	
 	public void validaTelefone(String telefone){
-		if(telefone.trim().isEmpty()){
-			throw new IllegalArgumentException("Telefone nao pode ser vazio");
-		}
-		if(telefone == null){
-			throw new NullPointerException("Telefone nao pode ser nulo");
-		}
+		this.validaString(telefone, "Telefone");
 	}
 	
 	public void validaEmail(String email){
-		if(email.trim().isEmpty()){
-			throw new IllegalArgumentException("Email nao pode ser vazio");
-		}
-		if(email == null){
-			throw new NullPointerException("Email nao pode ser nulo");
-		}
+		this.validaString(email, "Email");
 	}
 	
 	public void validaValor(String valor){
-		if(valor.trim().isEmpty()){
-			throw new IllegalArgumentException("Valor nao pode ser vazio");
-		}
-		if(valor == null){
-			throw new NullPointerException("Valor nao pode ser nulo");
-		}
+		this.validaString(valor, "Valor");
 	}
 	
 	public void validaNomeItem(String nomeItem){
-		if(nomeItem.trim().isEmpty()){
-			throw new IllegalArgumentException("Nome do item nao pode ser vazio");
-		}
-		if(nomeItem == null){
-			throw new NullPointerException("Nome do item nao pode ser nulo");
-		}
+		this.validaString(nomeItem, "Nome do item");
 	}
 	
 	public void validaDuracao(int duracao){
@@ -54,38 +38,37 @@ public class Validacoes {
 	}
 	
 	public void validaArtista(String artista){
-		if(artista.trim().isEmpty()){
-			throw new IllegalArgumentException("Artista nao pode ser vazio");
-		}
-		if(artista == null){
-			throw new NullPointerException("Artista nao pode ser nulo");
-		}
+		this.validaString(artista, "Artista");
 	}
 	
 	public void validaClassificacao(String classificacao){
-		if(classificacao.trim().isEmpty()){
-			throw new IllegalArgumentException("Classificacao nao pode ser vazia");
-		}
-		if(classificacao == null){
-			throw new NullPointerException("Classificacao nao pode ser nula");
-		}
+		this.validaString(classificacao, "Classificacao");
 	}
 	
 	public void validaNomeBluray(String nomeBluray){
-		if(nomeBluray.trim().isEmpty()){
-			throw new IllegalArgumentException("Nome do bluray nao pode ser vazio");
-		}
-		if(nomeBluray == null){
-			throw new NullPointerException("Nome do bluray nao pode ser nulo");
-		}
+		this.validaString(nomeBluray, "Nome do bluray");
 	}
 	
 	public void validaGenero(String genero){
-		if(genero.trim().isEmpty()){
-			throw new IllegalArgumentException("Genero nao pode ser vazio");
-		}
-		if(genero == null){
-			throw new NullPointerException("Genero nao pode ser nulo");
+		this.validaString(genero, "Genero");
+	}
+	
+	public void validaTemporada(String temporada){
+		this.validaString(temporada, "Temporada");
+	}
+	
+	public void validaPlataforma(String plataforma){
+		this.validaString(plataforma, "Plataforma");
+	}
+	
+	public void validaAnoLancamento(int ano){
+		if(ano <= 0){
+			throw new IllegalArgumentException("Ano nao pode ser negativo");
 		}
 	}
+	
+	public void validaData(String data){
+		this.validaString(data, "Data");
+	}
+	
 }
