830411c3dc8d9d97b69ceaf426f2326a4f259823
xdLuna <lunaft9@gmail.com>
2017-08-04 11:28:26 -0300

    Ajeitando a representacao dos enums

--- src/item/jogo/Plataforma.java
+++ src/item/jogo/Plataforma.java
@@ -2,7 +2,7 @@ package item.jogo;
 
 public enum Plataforma {
 
-	PC(""), MAC(""), PS3(""), PS4(""), XBOX360(""), XBOX_ONE(""), NINTENDO_3DS(""), OUTRO("");
+	PC("PC"), MAC("MAC"), PS3("PS3"), PS4("PS4"), XBOX360("XBOX360"), XBOX_ONE("XBOX_ONE"), NINTENDO_3DS("NINTENDO_3DS"), OUTRO("OUTRO");
 
 	private String plataforma;
 
