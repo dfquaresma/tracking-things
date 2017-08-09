a3b9b4be4266a6ef9d283084b01823759fad99ce
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2017-08-09 19:03:27 -0300

    Documentações das classes concluídas. Resta revisar.

--- src/item/bluray/Show.java
+++ src/item/bluray/Show.java
@@ -1,9 +1,9 @@
 package item.bluray;
 
 /**
- * Description: Os blu rays de shows devem indicar o nome do artista e o número
- * de faixas.
+ * Representação de um show em blu-ray.
  * 
+ * @author David Ferreira
  *
  */
 public class Show extends BluRay {
@@ -11,6 +11,23 @@ public class Show extends BluRay {
 	private int numDeFaixas;
 	private String artista;
 
+	/**
+	 * Constrói um show blu-ray com nome, preço, duração, numero de faixas,
+	 * artista e classificação.
+	 * 
+	 * @param nome
+	 *            o nome do show.
+	 * @param preco
+	 *            o preço do show.
+	 * @param duracao
+	 *            a duração do show.
+	 * @param numDeFaixas
+	 *            o número de faixas do show.
+	 * @param artista
+	 *            o artista do show.
+	 * @param classificacao
+	 *            a classificação do show.
+	 */
 	public Show(String nome, double valor, int duracao, int numDeFaixas, String artista, String classificacao) {
 		super(nome, valor, duracao, classificacao);
 		this.validador.validaNumeroDeFaixas(numDeFaixas);
@@ -20,24 +37,51 @@ public class Show extends BluRay {
 		this.artista = artista;
 	}
 
+	/**
+	 * Recupera o número de faixas do show.
+	 * 
+	 * @return um inteiro representando o número de faixas.
+	 */
 	public int getNumDeFaixas() {
 		return numDeFaixas;
 	}
 
+	/**
+	 * Atualiza o número de faixas do show.
+	 * 
+	 * @param numDeFaixas
+	 *            o novo número de faixas.
+	 */
 	public void setNumDeFaixas(int numDeFaixas) {
 		this.validador.validaNumeroDeFaixas(numDeFaixas);
 		this.numDeFaixas = numDeFaixas;
 	}
 
+	/**
+	 * Recupera o nome artista do show.
+	 * 
+	 * @return uma representação em String do nome do artista.
+	 */
 	public String getArtista() {
 		return artista;
 	}
 
+	/**
+	 * Atualiza o nome do artista do show.
+	 * 
+	 * @param artista
+	 *            o novo nome do artista.
+	 */
 	public void setArtista(String artista) {
 		this.validador.validaArtista(artista);
 		this.artista = artista;
 	}
 
+	/**
+	 * Sobrescreve o método da classe mãe para verificar se o atributo a ser
+	 * recuperado pertence a esta classe. Caso não seja, o método da classe mãe
+	 * é chamado.
+	 */
 	@Override
 	public String getInfo(String atributo) {
 		this.validador.validaAtributo(atributo);
@@ -53,6 +97,9 @@ public class Show extends BluRay {
 
 	}
 
+	/**
+	 * Retorna uma representação em string deste jogo.
+	 */
 	@Override
 	public String toString() {
 		return "SHOW: " + this.getNome() + ", R$ " + String.valueOf(this.getPreco())
@@ -60,6 +107,11 @@ public class Show extends BluRay {
 				+ this.getArtista() + ", " + this.getNumDeFaixas() + " faixas";
 	}
 	
+	/**
+	 * Sobrescreve o método da classe mãe para verificar se o atributo a ser
+	 * atualizado pertence a esta classe. Caso não seja, o método da classe mãe
+	 * é chamado.
+	 */
 	@Override
 	public void atualizaAtributo(String atributo, String valor) {
 		this.validador.validaAtributo(atributo);
--- src/item/bluray/Temporada.java
+++ src/item/bluray/Temporada.java
@@ -3,14 +3,9 @@ package item.bluray;
 import java.util.ArrayList;
 
 /**
- * Description: No blu ray de uma temporada você deve guardar a coleção de
- * blu-rays que compõem a temporada. A colecao de blu rays que representa uma
- * temporada de uma série deve manter, além da colecao de blu rays, o genero da
- * serie e o numero da temporada.
+ * Representação de uma temporada de série com episódios.
  * 
- * Uma temporada so eh emprestavel se possuir todos os seus episodios em
- * blue-rays (Voces decidiram que nao sera possivel emprestar um único blu ray
- * de uma temporada, ou empresta a temporada inteira ou não empresta nada)
+ * @author David Ferreira
  *
  */
 public class Temporada extends BluRay {
@@ -20,6 +15,26 @@ public class Temporada extends BluRay {
 	private int temporada;
 	private ArrayList<Episodio> episodios;
 
+	/**
+	 * Constrói uma temporada com nome, preço, descrição, duração,
+	 * classificação, gênero e temporada (edição dessa temporada. Ex: temporada
+	 * 1, temporada 2....).
+	 * 
+	 * @param nome
+	 *            o nome da temporada.
+	 * @param preco
+	 *            o preço da série.
+	 * @param descricao
+	 *            a descrição da série.
+	 * @param duracao
+	 *            a duração da série.
+	 * @param classificacao
+	 *            a classificação da série.
+	 * @param genero
+	 *            o gênero da série.
+	 * @param temporada
+	 *            a edição da temporada.
+	 */
 	public Temporada(String nome, double valor, String descricao, int duracao, String classificacao, String genero,
 			int temporada) {
 		super(nome, valor, duracao, classificacao);
@@ -33,39 +48,82 @@ public class Temporada extends BluRay {
 		this.episodios = new ArrayList<Episodio>();
 	}
 
+	/**
+	 * Recupera a descrição da temporada.
+	 * 
+	 * @return uma string representando a descrição.
+	 */
 	public String getDescricao() {
 		return descricao;
 	}
 
+	/**
+	 * Atualiza a descrição da temporada.
+	 * 
+	 * @param descricao
+	 *            a nova descrição da temporada.
+	 */
 	public void setDescricao(String descricao) {
 		this.validador.validaDescricao(descricao);
 		this.descricao = descricao;
 	}
 
+	/**
+	 * Recupera o gênero da temporada.
+	 * 
+	 * @return uma string representando o gênero.
+	 */
 	public String getGenero() {
 		return genero.toString();
 	}
 
+	/**
+	 * Atualiza o gênero da temporada.
+	 * 
+	 * @param genero
+	 *            o novo gênero.
+	 */
 	public void setGenero(String genero) {
 		this.validador.validaGenero(genero);
 		this.genero = Genero.valueOf(genero);
 	}
 
+	/**
+	 * Recupera o número da temporada.
+	 * 
+	 * @return um inteiro indicando a temporada.
+	 */
 	public int getTemporada() {
 		return temporada;
 	}
 
+	/**
+	 * Atualiza o número da temporada.
+	 * 
+	 * @param temporada
+	 *            o novo número da temporada.
+	 */
 	public void setTemporada(int temporada) {
 		this.validador.validaTemporada(temporada);
 		this.temporada = temporada;
 	}
 
+	/**
+	 * Adiciona um episódio á temporada.
+	 * 
+	 * @param duracao
+	 *            a duração do episódio a ser adicionado.
+	 */
 	public void addBlueray(int duracao) {
 		this.validador.validaDuracao(duracao);
-		// NAO COMPLETO
 		this.episodios.add(new Episodio(duracao));
 	}
 
+	/**
+	 * Sobrescreve o método da classe mãe para verificar se o atributo a ser
+	 * recuperado pertence a esta classe. Caso não seja, o método da classe mãe
+	 * é chamado.
+	 */
 	@Override
 	public String getInfo(String atributo) {
 		this.validador.validaAtributo(atributo);
@@ -83,6 +141,11 @@ public class Temporada extends BluRay {
 
 	}
 
+	/**
+	 * Recupera um booleano informando se o item pode ser emprestado.
+	 * 
+	 * @return true caso possa ser emprestado, false caso contrário.
+	 */
 	@Override
 	public boolean podeSerEmprestado() {
 		int duracaoTotalEpisodios = 0;
@@ -93,6 +156,9 @@ public class Temporada extends BluRay {
 		return (duracaoTotalEpisodios == super.getDuracao()) && super.podeSerEmprestado();
 	}
 
+	/**
+	 * Retorna uma representação em string deste jogo.
+	 */
 	@Override
 	public String toString() {
 		return "SERIE: " + this.getNome() + ", R$ " + String.valueOf(this.getPreco()) + ", "
@@ -100,6 +166,11 @@ public class Temporada extends BluRay {
 				+ this.getGenero() + ", Temporada " + this.getTemporada();
 	}
 	
+	/**
+	 * Sobrescreve o método da classe mãe para verificar se o atributo a ser
+	 * atualizado pertence a esta classe. Caso não seja, o método da classe mãe
+	 * é chamado.
+	 */
 	@Override
 	public void atualizaAtributo(String atributo, String valor) {
 		this.validador.validaAtributo(atributo);
