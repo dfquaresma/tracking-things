59f5603c18aab0f80d169e3536e22b3026ffd3a9
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2017-08-10 22:32:41 -0300

    Complementando testes de Filme

--- teste/item/bluray/FilmeTest.java
+++ teste/item/bluray/FilmeTest.java
@@ -15,45 +15,86 @@ public class FilmeTest {
 		this.filmeTest = new Filme("Senhor dos anéis", 220, 150, "ACAO", "DEZ_ANOS", 2002);
 	}
 	
-	@Test
-	public void testGetsEspecificos() {
-
-		assertEquals("Senhor dos anéis", filmeTest.getNome());
-		assertEquals(220, filmeTest.getPreco(), 0.01);
-		assertEquals(150, filmeTest.getDuracao());
-		assertEquals("DEZ_ANOS", filmeTest.getClassificacao());
-
-		assertEquals("Senhor dos anéis", filmeTest.getInfo("Nome"));
-		assertEquals("DEZ_ANOS", filmeTest.getInfo("Classificacao"));
-
-		try {
-			filmeTest.getInfo(null);
-			fail();
-		} catch (NullPointerException npe) {
-			assertEquals("Atributo nao pode ser nulo", npe.getMessage());
-		}
-
-		try {
-			filmeTest.getInfo("");
-			fail();
-		} catch (IllegalArgumentException npe) {
-			assertEquals("Atributo nao pode ser vazio", npe.getMessage());
-		}
-
+	@Test(expected=NullPointerException.class)
+	public void testFilmeNomeNull()
+	{
+		new Filme(null, 220, 150, "ACAO", "DEZ_ANOS", 2002);
 	}
 	
-	public void testSetGenero() {
+	@Test(expected=IllegalArgumentException.class)
+	public void testFilmeNomeVazio()
+	{
+		new Filme(" ", 220, 150, "ACAO", "DEZ_ANOS", 2002);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testFilmeValorInvalido()
+	{
+		new Filme("Senhor dos anéis", -0.0001, 150, "ACAO", "DEZ_ANOS", 2002);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testFilmeDuracaoInvalida()
+	{
+		new Filme("Senhor dos anéis", 220, 0, "ACAO", "DEZ_ANOS", 2002);
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void testFilmeGeneroNull()
+	{
+		new Filme("Senhor dos anéis", 220, 150, null, "DEZ_ANOS", 2002);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testFilmeGeneroVazio()
+	{
+		new Filme("Senhor dos anéis", 220, 150, " ", "DEZ_ANOS", 2002);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testFilmeGeneroInvalido()
+	{
+		new Filme("Senhor dos anéis", 220, 150, "acao", "DEZ_ANOS", 2002);
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void testFilmeClassificacaoNull()
+	{
+		new Filme("Senhor dos anéis", 220, 150, "ACAO", null, 2002);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testFilmeClassificacaoVazia()
+	{
+		new Filme("Senhor dos anéis", 220, 150, "ACAO", " ", 2002);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testFilmeClassificacaoInvalida()
+	{
+		new Filme("Senhor dos anéis", 220, 150, "ACAO", "quinze_", 2002);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testFilmeAnoLancamentoInvalido()
+	{
+		new Filme("Senhor dos anéis", 220, 150, "ACAO", "DEZ_ANOS", 0);
+	}
+	
+	@Test
+	public void testSetGenero()
+	{
 		try {
 			filmeTest.setGenero(null);
 			fail();
 		} catch (NullPointerException npe) {
-			assertEquals("Valor nao pode ser nulo", npe.getMessage());
+			assertEquals("Genero nao pode ser nulo", npe.getMessage());
 		}
 		try {
 			filmeTest.setGenero("");
 			fail();
 		} catch (IllegalArgumentException iae) {
-			assertEquals("Valor nao pode ser vazio", iae.getMessage());
+			assertEquals("Genero nao pode ser vazio", iae.getMessage());
 		}
 		try {
 			filmeTest.setGenero("ZERO_ANOS");
@@ -64,131 +105,11 @@ public class FilmeTest {
 		filmeTest.setGenero("DRAMA");
 		assertEquals("DRAMA", filmeTest.getGenero());
 		
-	} 
-	
-	public void testSetAnoLancamento() {
-		try {
-			filmeTest.setAnoDeLancamento(0);
-			fail();
-		} catch (IllegalArgumentException iae) {
-			assertEquals("Ano de lancamento nao pode ser menor ou igual a zero", iae.getMessage());
-		}
-		filmeTest.setAnoDeLancamento(10);
-		assertEquals(10, filmeTest.getAnoDeLancamento());
-		
 	}
 	
 	@Test
-	public void testSetNome() {
-		try {
-			filmeTest.setNome(null);
-			fail();
-		} catch (NullPointerException iae) {
-			assertEquals("Nome nao pode ser nulo", iae.getMessage());
-		}
-		try {
-			filmeTest.setNome("");
-			fail();
-		} catch (IllegalArgumentException iae) {
-			assertEquals("Nome nao pode ser vazio", iae.getMessage());
-		}
-		filmeTest.setNome("NomeTest1");
-		assertEquals("NomeTest1", filmeTest.getNome());
-
-	}
-
-	@Test
-	public void testSetPreco() {
-		try {
-			filmeTest.setPreco(0);
-			fail();
-		} catch (IllegalArgumentException iae) {
-			assertEquals("Preco invalido", iae.getMessage());
-		}
-		filmeTest.setPreco(100);
-		assertEquals(100, filmeTest.getPreco(), 0.01);
-	}
-
-	@Test
-	public void testSetDuracao() {
-		try {
-			filmeTest.setDuracao(0);
-			fail();
-		} catch (IllegalArgumentException iae) {
-			assertEquals("Duracao nao pode ser menor ou igual a zero", iae.getMessage());
-		}
-		filmeTest.setDuracao(10);
-		assertEquals(10, filmeTest.getDuracao());
-	}
-
-	@Test
-	public void testSetClassificacao() {
-		try {
-			filmeTest.setClassificacao(null);
-			fail();
-		} catch (NullPointerException npe) {
-			assertEquals("Classificacao nao pode ser nulo", npe.getMessage());
-		}
-		try {
-			filmeTest.setClassificacao("");
-			fail();
-		} catch (IllegalArgumentException iae) {
-			assertEquals("Classificacao nao pode ser vazio", iae.getMessage());
-		}
-		try {
-			filmeTest.setClassificacao("ZERO_ANOS");
-			fail();
-		} catch (IllegalArgumentException iae) {
-			assertEquals("Classificacao inválida", iae.getMessage());
-		}
-		filmeTest.setClassificacao("DOZE_ANOS");
-		assertEquals("DOZE_ANOS", filmeTest.getClassificacao());
-	}
-
-	@Test
-	public void testAtualizaAtributoNome() {
-		try {
-			filmeTest.atualizaAtributo("Nome", null);
-			fail();
-		} catch (NullPointerException iae) {
-			assertEquals("Valor nao pode ser nulo", iae.getMessage());
-		}
-		try {
-			filmeTest.atualizaAtributo("Nome", "");
-			fail();
-		} catch (IllegalArgumentException iae) {
-			assertEquals("Valor nao pode ser vazio", iae.getMessage());
-		}
-		filmeTest.atualizaAtributo("Nome", "Outro");
-		
-	}
-
-	@Test
-	public void testAtualizaAtributoPreco() {
-		try {
-			filmeTest.atualizaAtributo("Preco", "-1");
-			fail();
-		} catch (IllegalArgumentException iae) {
-			assertEquals("Valor nao pode ser menor ou igual a zero para essa operacao", iae.getMessage());
-		}
-		filmeTest.atualizaAtributo("Preco", "20");
-		
-	}
-	
-	@Test
-	public void testAtualizaAtributoDuracao() {
-		try {
-			filmeTest.atualizaAtributo("Duracao", "0");
-			fail();
-		} catch (IllegalArgumentException iae) {
-			assertEquals("Valor nao pode ser menor ou igual a zero para essa operacao", iae.getMessage());
-		}
-		filmeTest.atualizaAtributo("Duracao", "20");
-
-	}
-
-	@Test
-	public void testAtualizaAtributoAnoLancamento() {
+	public void testSetAnoLancamento()
+	{
 		try {
 			filmeTest.setAnoDeLancamento(0);
 			fail();
@@ -199,71 +120,104 @@ public class FilmeTest {
 		assertEquals(10, filmeTest.getAnoDeLancamento());
 		
 	}
-
+	
+	//new Filme("Senhor dos anéis", 220, 150, "ACAO", "DEZ_ANOS", 2002);
+	
 	@Test
-	public void testAtualizaAtributoClassificacao() {
-		
-
-		try {
-			filmeTest.atualizaAtributo("Classificacao", null);
-			fail();
-		} catch (NullPointerException npe) {
-			assertEquals("Valor nao pode ser nulo", npe.getMessage());
-		}
-		try {
-			filmeTest.atualizaAtributo("Classificacao", "");
-			fail();
-		} catch (IllegalArgumentException iae) {
-			assertEquals("Valor nao pode ser vazio", iae.getMessage());
-		}
-		try {
-			filmeTest.atualizaAtributo("Classificacao", "ZERO_ANOS");
-			fail();
-		} catch (IllegalArgumentException iae) {
-			assertEquals("Classificacao inválida", iae.getMessage());
-		}
-		filmeTest.atualizaAtributo("Classificacao", "DOZE_ANOS");
-		
+	public void testGetInfo()
+	{
+		assertEquals("ACAO", this.filmeTest.getInfo("Genero"));
+		assertEquals("2002", this.filmeTest.getInfo("Lancamento"));
+		assertEquals("Senhor dos anéis", this.filmeTest.getInfo("Nome"));
 	}
-
-	@Test
-	public void testAtualizaAtributoGenero() {
-		try {
-			filmeTest.atualizaAtributo("Genero", null);
-			fail();
-		} catch (NullPointerException npe) {
-			assertEquals("Valor nao pode ser nulo", npe.getMessage());
-		}
-		try {
-			filmeTest.atualizaAtributo("Genero", "");
-			fail();
-		} catch (IllegalArgumentException iae) {
-			assertEquals("Valor nao pode ser vazio", iae.getMessage());
-		}
-		try {
-			filmeTest.atualizaAtributo("Genero", "ZERO_ANOS");
-			fail();
-		} catch (IllegalArgumentException iae) {
-			assertEquals("Genero inválido", iae.getMessage());
-		}
-		filmeTest.atualizaAtributo("Genero", "DRAMA");
-
+	
+	@Test(expected=NullPointerException.class)
+	public void testGetInfoAtributoNull()
+	{
+		this.filmeTest.getInfo(null);
 	}
-
-	@Test
-	public void testAtualizaAtributoInvalido() {
-		try {
-			filmeTest.atualizaAtributo("Outro", "-1");
-			fail();
-		} catch (IllegalArgumentException iae) {
-			assertEquals("Atributo invalido", iae.getMessage());
-		}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testGetInfoAtributoVazio()
+	{
+		this.filmeTest.getInfo("  ");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testGetInfoAtributoInvalido()
+	{
+		this.filmeTest.getInfo("naosouumatributo");
 	}
 	
 	@Test
-	public void testToString() {
+	public void testToString()
+	{
 		assertEquals("FILME: Senhor dos anéis, R$ 220.0, Nao emprestado, 150 min, DEZ_ANOS, ACAO, 2002", filmeTest.toString());
+	}
+	
+	@Test
+	public void testGetsEspecificos() {
 
+		assertEquals("Senhor dos anéis", filmeTest.getNome());
+		assertEquals(220, filmeTest.getPreco(), 0.01);
+		assertEquals(150, filmeTest.getDuracao());
+		assertEquals("DEZ_ANOS", filmeTest.getClassificacao());
+
+		assertEquals("Senhor dos anéis", filmeTest.getInfo("Nome"));
+		assertEquals("DEZ_ANOS", filmeTest.getInfo("Classificacao"));
+	}
+
+	@Test
+	public void testAtualizaAtributo()
+	{
+		this.filmeTest.atualizaAtributo("Genero", "COMEDIA");
+		assertEquals("COMEDIA", this.filmeTest.getGenero());
+		this.filmeTest.atualizaAtributo("Ano de Lancamento", "2010");
+		assertEquals(2010, this.filmeTest.getAnoDeLancamento());
+		this.filmeTest.atualizaAtributo("Duracao", "150");
+		assertEquals(150, this.filmeTest.getDuracao());
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void testAtualizaAtributoNull()
+	{
+		this.filmeTest.atualizaAtributo(null, "COMEDIA");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testAtualizaAtributoVazio()
+	{
+		this.filmeTest.atualizaAtributo(" ", "COMEDIA");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testAtualizaAtributoInvalido()
+	{
+		this.filmeTest.atualizaAtributo("naosouatributo", "COMEDIA");
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void testAtualizaValorNull()
+	{
+		this.filmeTest.atualizaAtributo("Genero", null);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testAtualizaValorVazio()
+	{
+		this.filmeTest.atualizaAtributo("Ano de Lancamento", " ");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testAtualizaAnoLancamentoNaoInt()
+	{
+		this.filmeTest.atualizaAtributo("Ano de Lancamento", "doismiledois");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testAtualizaAtributoGeneroInvalido()
+	{
+		this.filmeTest.atualizaAtributo("Genero", "naosoudotipogenero");
 	}
 
 }
