787363c6ee3b978650360ca5100d880fea3769e7
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2017-08-18 15:49:25 -0300

    esqueleto de testes adicionado.

---
+++ teste/emprestimo/comparador/EmprestimosComparatorDataEmprestimoTest.java
@@ -0,0 +1,14 @@
+package emprestimo.comparador;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EmprestimosComparatorDataEmprestimoTest {
+
+	@Test
+	public void testCompare() {
+		fail("Not yet implemented");
+	}
+
+}
