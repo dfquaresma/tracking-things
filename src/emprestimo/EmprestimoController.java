f4b237e68681bf9186c7b947d26ca0c1a9ff620c
Ícaro Lima <icaro.lima@ccc.ufcg.edu.br>
2017-08-20 14:28:34 -0300

    Retirei imports desnecessários.

--- src/emprestimo/EmprestimoController.java
+++ src/emprestimo/EmprestimoController.java
@@ -1,10 +1,7 @@
 package emprestimo;
 
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 import item.Item;
 import usuario.Usuario;
