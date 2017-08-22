9d67917db4308281511067f15c53c9ede8adf6b5
xdLuna <lunaft9@gmail.com>
2017-08-21 21:02:32 -0300

    Documentacao dos cartoes fidelidade

--- src/usuario/fidelidade/Caloteiro.java
+++ src/usuario/fidelidade/Caloteiro.java
@@ -1,19 +1,33 @@
 package usuario.fidelidade;
-
+/**
+ * Representacao da categoria Caloteiro do cartao fidelidade
+ *
+ * @author Amanda V. A. de Luna e Costa
+ * @author David Ferreira Quaresma
+ * @author Ícaro Dantas de Araújo Lima
+ * @author Paulo Felipe Feitosa da Silva
+ *
+ */
 public class Caloteiro implements CartaoFidelidade {
 
 	private final int PERIODO = 0;
-
+	/**
+	 * Retorna o periodo que a categoria pode pegar um item emprestado
+	 */
 	@Override
 	public int getPeriodo() {
 		return this.PERIODO;
 	}
-
+	/**
+	 * Retorna se a categoria pode pegar itens emprestado
+	 */
 	@Override
 	public boolean podePegarEmprestado() {
 		return false;
 	}
-
+	/**
+	 * Retorna a representacao em string da categoria
+	 */
 	@Override
 	public String toString() {
 		return "Caloteiro";
